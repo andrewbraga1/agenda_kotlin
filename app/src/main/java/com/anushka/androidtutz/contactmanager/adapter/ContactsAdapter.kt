b76cc5464ea0911a161c7bee2d99ec853bfdd730
tliradiniz <tdiniz22@gmail.com>
2019-05-28 22:49:43 -0300

    First commit

---
+++ app/src/main/java/com/anushka/androidtutz/contactmanager/adapter/ContactsAdapter.kt
@@ -0,0 +1,59 @@
+package com.anushka.androidtutz.contactmanager.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+import com.anushka.androidtutz.contactmanager.MainActivity
+import com.anushka.androidtutz.contactmanager.R
+import com.anushka.androidtutz.contactmanager.db.entity.Contact
+
+import java.util.ArrayList
+
+
+class ContactsAdapter(private val context: Context, private val contactssList: ArrayList<Contact>, private val mainActivity: MainActivity) : RecyclerView.Adapter<ContactsAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        var name: TextView
+        var emil: TextView
+
+
+        init {
+            name = view.findViewById(R.id.name)
+            emil = view.findViewById(R.id.email)
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.contact_list_item, parent, false)
+
+        return MyViewHolder(itemView)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+
+        val contact = contactssList[position]
+
+        holder.name.text = contact.name
+        holder.emil.text = contact.email
+
+        holder.itemView.setOnClickListener { mainActivity.addAndEditContacts(true, contact, position) }
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return contactssList.size
+    }
+
+
+}
+
